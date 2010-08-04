52e679747a66cfef7da3f08e0d152d8d4da57d4d
wincent (HUANG RONGGUI) <ronggui.huang@gmail.com>
2010-08-04 01:53:44 +0000

    add ikanalyser
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rqda@334 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/RQDAtm/R/CWS.R
+++ pkg/RQDAtm/R/CWS.R
@@ -1,9 +1,9 @@
 ## Chinese Word Segmentation
-CWS <- function (text,stpwords=FALSE)
+CWS <- CWSimdict <- function (text,stpwords=FALSE)
 {
     if (length(text)!= 1) stop("text must be length-1 character vector.")
     if (isTRUE(stpwords)) zj <- "true" else zj <- "false"
-    .jinit(system.file("plugins","ChineseWordSegmentation.jar",package="RQDAtm"))
+    .jinit(system.file("plugins","CWSimdict.jar",package="RQDAtm"))
     ## the plugins are from http://code.google.com/p/imdict-chinese-analyzer/
     hjw <- .jnew("org.apache.lucene.analysis.cn.hzfc")
     outRef <- .jcall(hjw, "S" , "txtMethod",text,zj, evalString = FALSE)
@@ -11,9 +11,9 @@ CWS <- function (text,stpwords=FALSE)
     ans
 }
 
-## Chinese Word Segmentation
 CWSmmseg <- function (text)
 {
+## http://code.google.com/p/mmseg4j/
     if (length(text)!= 1) stop("text must be length-1 character vector.")
     .jinit(system.file("plugins","CWSmmseg4j.jar",package="RQDAtm"))
     mmseg <- .jnew("com/chenlb/mmseg4j/example/CWSmmseg")
@@ -21,4 +21,16 @@ CWSmmseg <- function (text)
     ans <- .jstrVal(outRef)
     ans
 }
+
+CWSik <- function (text)
+{
+## http://code.google.com/p/ik-analyzer/
+    if (length(text)!= 1) stop("text must be length-1 character vector.")
+    .jinit(system.file("plugins","IKCWS.jar",package="RQDAtm"))
+    ikcws <- .jnew("org/wltea/analyzer/example/IKCWS")
+    outRef <- .jcall(ikcws, "S" , "segWords",text,evalString = FALSE)
+    ans <- .jstrVal(outRef)
+    ans
+}
+
 
--- pkg/RQDAtm/R/tokenizeFun.R
+++ pkg/RQDAtm/R/tokenizeFun.R
@@ -1,18 +1,18 @@
-tokenizeByDictionary <- function(x,dictionary=NULL,...){
-    ## x id Corpus[[i]]
-    ## dictionary is a tm::Dictionary
-    ## this function extract dictionary as term from a Corpus to form a doc x term matrix.
-    require(gsubfn)
-    if (is.null(dictionary)){
-        if (exists("control",parent.frame())) {
-            envir <- parent.frame()
-        } else if (exists("control",parent.frame(2))) envir <- parent.frame(2)
-        dictionary <- evalq(control$dictionar,envir=envir)
-    }
-    if (is.null(dictionary)) stop("dictionary must be provided.")
-    ans <- unlist(sapply(as.list(dictionary), function(ii)strapply(x,ii)))
-    ans
-}
+##tokenizeByDictionary <- function(x,dictionary=NULL,...){
+##    ## x id Corpus[[i]]
+##    ## dictionary is a tm::Dictionary
+##    ## this function extract dictionary as term from a Corpus to form a doc x term matrix.
+##    require(gsubfn)
+##    if (is.null(dictionary)){
+##        if (exists("control",parent.frame())) {
+##            envir <- parent.frame()
+##        } else if (exists("control",parent.frame(2))) envir <- parent.frame(2)
+##        dictionary <- evalq(control$dictionar,envir=envir)
+##    }
+##    if (is.null(dictionary)) stop("dictionary must be provided.")
+##    ans <- unlist(sapply(as.list(dictionary), function(ii)strapply(x,ii)))
+##    ans
+##}
 
 ## example
 ##termFreq(u[[1]],list(tokenize=function(x)tokenize(x),minWordLength=1,dictionary=d))
