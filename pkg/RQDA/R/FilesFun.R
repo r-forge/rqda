18789d409e74d019d187dcfce6e63ec1c34b738b
wincent (HUANG RONGGUI) <ronggui.huang@gmail.com>
2011-09-07 15:57:03 +0000

    fix bugs ("find a word" has highlight again; project memo button is grayed by default)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rqda@374 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/RQDA/R/FilesFun.R
+++ pkg/RQDA/R/FilesFun.R
@@ -648,6 +648,7 @@ searchWord <- function(str,widget,from=0,col="green"){
     ans <- gtkTextIterForwardSearch(Iter0,str,'GTK_TEXT_SEARCH_VISIBLE_ONLY')
     if (ans$retval) {
         gtkTextViewScrollToIter(tview,ans$match.start,0.47)
+        buffer$createTag(sprintf("%s.background",col),background = col)
         buffer$ApplyTagByName(sprintf("%s.background", col),ans$match.start, ans$match.end)
         ans$match.end$GetOffset()
     } else {
