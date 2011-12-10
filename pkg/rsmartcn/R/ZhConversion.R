ce1bbcf5c2067af8f3c7ba760c417028d848df4a
wincent (HUANG RONGGUI) <ronggui.huang@gmail.com>
2011-12-10 02:51:20 +0000

    move CWS from RQDAtm; fix a bug in zhConv
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rqda@420 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/rsmartcn/R/ZhConversion.R
+++ pkg/rsmartcn/R/ZhConversion.R
@@ -26,15 +26,15 @@ dicGen <- function(file,encoding="UTF-8"){
 
 ## dic <- dicGen("http://svn.wikimedia.org/svnroot/mediawiki/trunk/phase3/includes/ZhConversion.php")
 
-zhConv <-  function(string,dic=dic$zh2Hans) {
+zhConv <-  function(string,dictionary=dic$zh2Hans) {
     ## dic must be a UTF8 character.
     i = 1
     string2 <- strsplit(string,"")[[1]]
     while (i < length(string2)) {
         for (j in seq(from=length(string2),to=i)) {
             item <- paste(string2[seq(from=i,to=j)],collapse="")
-            if  (any(dic[,1]==item)) {
-                sub = dic[,2][ dic[,1] ==item]
+            if  (any(dictionary[,1]==item)) {
+                sub = dictionary[,2][dictionary[,1] ==item]
                 sub <- strsplit(sub,"")[[1]]
                 if (j >= length(string2))  string2 <- c(string2[seq(from=0,to=i-1)], sub) else {
                    string2 <- c(string2[seq(from=0,to=i-1)], sub, string2[seq(from=j+1,to=length(string2))])
