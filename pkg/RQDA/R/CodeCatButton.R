17f6b5d432efe55b6231e95e9e12ba92c7a5e36f
wincent (HUANG RONGGUI) <ronggui.huang@gmail.com>
2012-04-13 06:54:21 +0000

    retrieve codings of selected code category
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rqda@426 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/RQDA/R/CodeCatButton.R
+++ pkg/RQDA/R/CodeCatButton.R
@@ -213,6 +213,33 @@ and treecode.catid=codecat.catid and freecode.id=treecode.cid and codecat.name i
 }
 
 
+getCodindsByCategory <- function(catid=NULL, fid = NULL, codingTable = c("coding", "coding2")){
+    if (is.null(catid)) catid <- RQDAQuery(sprintf("select catid from codecat where name = '%s'", enc(svalue(.rqda$.CodeCatWidget))))$catid
+    cid <- RQDAQuery(sprintf("select cid from treecode where catid==%s and status==1",catid))$cid
+    codingTable <- match.arg(codingTable)
+    if (codingTable == "coding") {
+        ct <- RQDAQuery(sprintf("select coding.rowid as rowid, coding.cid, coding.fid,
+freecode.name as codename, source.name as filename, coding.selfirst as index1,
+coding.selend as index2, coding.seltext as coding, coding.selend - coding.selfirst as CodingLength from coding
+ left join freecode on (coding.cid=freecode.id) left join source on (coding.fid=source.id)
+where coding.status=1 and source.status=1 and freecode.status=1 and coding.cid in (%s)", paste(cid,collapse=",")))
+    }
+    if (codingTable == "coding2") {
+        ct <- RQDAQuery(sprintf("select coding.rowid as rowid, coding.cid, coding.fid,
+freecode.name as codename, source.name as filename, coding2.selfirst as index1,
+coding2.selend as index2, coding2.seltext as coding2, coding2.selend - coding2.selfirst as CodingLength from coding2
+ left join freecode on (coding2.cid=freecode.id) left join source on (coding2.fid=source.id)
+where coding2.status=1 and source.status=1 and freecode.status=1 and coding2.cid in (%s)", paste(cid,collapse=",")))
+    }
+    if (nrow(ct) != 0) {
+        Encoding(ct$codename) <- Encoding(ct$filename) <- Encoding(ct$coding) <- "UTF-8"
+        if (!is.null(fid))
+            ct <- ct[ct$fid %in% fid, ]
+    }
+    ## class(ct) <- c("codingsByOne", "data.frame")
+    ct
+}
+
 CodeCatWidgetMenu <- list()
 CodeCatWidgetMenu$"Add New Code to Selected Category"$handler <- function(h,...) {
     if (is_projOpen(env=.rqda,conName="qdacon")) {
@@ -234,6 +261,12 @@ CodeCatWidgetMenu$"Add New Code to Selected Category"$handler <- function(h,...)
                 UpdateCodeofCatWidget()
             }
         }
+    }
+}
+CodeCatWidgetMenu$"Codings of selected category"$handler <- function(h,...){
+    if (is_projOpen(env=.rqda,conName="qdacon")) {
+        ct <- getCodindsByCategory()
+        print.codingsByOne(ct)
     }
 }
 CodeCatWidgetMenu$Memo$handler <- function(h,...){
